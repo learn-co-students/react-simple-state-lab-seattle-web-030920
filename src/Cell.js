b4f653b546d7e081a0fabdc0639c726c7d172731
theouty <aman.david.michael@gmail.com>
2020-05-16 16:48:30 -0700

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,16 +1,64 @@
-import React, {Component} from 'react'
-class Cell extends Component{
-constructor(props){
-    this.state ={color: props.value}
-}
-handleClick(){
-    this.setState ={color: '#333'}
-}
-render(){
-    return(
-        <div> onClick = {this.handleClick()} className ="cell" style={{backgroundColor: this.state.color}}</div>
-    )
-}
+import React, { Component } from "react";
+
+export default class Cell extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      color: props.value,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: "#333",
+    });
+  };
+
+  render() {
+    return (
+      <div
+        className="cell"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      ></div>
+    );
+  }
 }
 
-export default Cell
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {Component} from 'react'
+// class Cell extends Component{
+// constructor(props){
+//     this.state = {color: props.value}
+// }
+// handleClick(){
+//     this.setState ={color: '#333'}
+// }
+// render(){
+//     return(
+//         <div> onClick = {this.handleClick()} className ="cell" style={{backgroundColor: this.state.color}}</div>
+//     )
+// }
+// }
+
+// export default Cell
